111f4dbc8d48b081965b2a6ef8d8dcca1574dff7
Rasheen <sheen.ruwisha@gmail.com>
2020-09-21 17:05:16 +0530

    Initial check-in of module Calculator

---
+++ Calculator/Calculator.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Calculator
+{
+    public class Calculator
+    {
+        public static Result DoOperation(double num1, double num2, string op)
+        {
+            Result result = new Result
+            {
+                result = double.NaN,
+                op = ""
+            };
+
+            switch (op)
+            {
+                case "a":
+                    result.result = num1 + num2;
+                    result.op = "+";
+                    break;
+                case "s":
+                    result.result = num1 - num2;
+                    result.op = "-";
+                    break;
+                case "m":
+                    result.result = num1 * num2;
+                    result.op = "*";
+                    break;
+                case "d":
+                    if (num2 != 0)
+                    {
+                        result.result = num1 / num2;
+                        result.op = "/";
+                    }
+                    break;
+                case "r":
+                    result.result = num1 % num2;
+                    result.op = "%";
+                    break;
+                default:
+                    break;
+            }
+            return result;
+        }
+
+
+    }
+}
---
+++ Calculator/Result.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace Calculator
+{
+    public class Result
+    {
+
+        public double result { get; set; }
+
+        public string op { get; set; }
+
+    }
+}
